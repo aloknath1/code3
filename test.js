57db777da836aad3304ab18d0d474c5f1d309605
Alok <nath.alok59@gmail.com>
2016-01-12 22:30:20 +0530

    toggle

--- test.js
+++ test.js
@@ -1,4 +1,36 @@
-.on('click',function(){
-var selector = $(this).data('target');
-$(selector).toggle();
-});
+var listbodystatus = 1;
+var globalid = '';
+var vis = d3.select(".listbody")
+			.selectAll("div")
+			.data(data)
+			.enter()
+			.append("div")
+			.attr("class","app")
+			.attr("data-toggle","toggle")
+			.attr("data-target","#demo")
+			.attr("id",function(d,i){ return "div"+i;})
+			.on("click",function(d){
+			
+				d3.selectAll(".listbody div").classed("active",false);
+				var selector = $(this).data("target");
+				if(listbodystatus === 1 && globalid == '')
+				{
+					globalid = $(this).id;
+					$(selector).toggleClass("in");
+					$(this).toggleClass("active");
+					listbodystatus++;
+					
+				}else if(globalid == $(this).id)
+				{
+					$(selector).toggleClass("in");
+					listbodystatus = 1;
+				}else{
+					$(selector).removeClass('in');
+					$(this).toggleClass('active');
+					setTimeout(function(){
+						
+						$(selector).addClass('in');						
+						$(selector).html("abc");
+					},1400);
+				}
+			})
